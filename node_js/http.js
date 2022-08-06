0e7a600bf17013f1f6a624411bd380c1a5490500
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-08-06 18:45:47 +0530

    nodemon installed

--- node_js/http.js
+++ node_js/http.js
@@ -2,7 +2,7 @@
 A protocol is system of rules that we define how data is exchanged within or between computers.
 
 HTTP is a protocol for fetching resources such as HTML documents. It is the foundation of any data exchange on the Web and it is a client-server protocol, which means requests are initiated by the recipient, usually the Web browser. A complete document is reconstructed from the different sub-documents fetched, for instance, text, layout description, images, videos, scripts, and more.
-
+The messages sent by the client, usually a Web browser, are called requests and the messages sent by the server as an answer are called responses.
 
 
 */
