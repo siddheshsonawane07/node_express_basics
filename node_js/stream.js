e4f0a7a7737700ceee0c811cb1276d998ec788e1
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-08-06 23:30:02 +0530

    stream js

---
+++ node_js/stream.js
@@ -0,0 +1,13 @@
+/*
+A stream is an abstract interface for working with streaming data in Node.js. The node:stream module provides an API for implementing the stream interface.
+
+Types of streams#
+There are four fundamental stream types within Node.js:
+
+Writable: streams to which data can be written (for example, fs.createWriteStream()).
+Readable: streams from which data can be read (for example, fs.createReadStream()).
+Duplex: streams that are both Readable and Writable (for example, net.Socket).
+Transform: Duplex streams that can modify or transform the data as it is written and read (for example, zlib.createDeflate()).
+Additionally, this module includes the utility functions stream.pipeline(), stream.finished(), stream.Readable.from() and stream.addAbortSignal().
+
+*/
