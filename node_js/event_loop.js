464cf64c3fd33c3207b49bb09f0be3915fdb6acf
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-08-06 20:14:24 +0530

    event_loops

---
+++ node_js/event_loop.js
@@ -0,0 +1,8 @@
+ /*
+Javscript is a single threaded programming language, single threaded runtime which means it has single call stack
+
+https://www.youtube.com/watch?v=8aGhZQkoFbQ by Philips Roberts
+JavaScript has a runtime model based on an event loop, which is responsible for executing the code, collecting and processing events, and executing queued sub-tasks.
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/EventLoop
+ */
