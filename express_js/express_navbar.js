04050a85b935717fd10d4ba1926d268f65b091d6
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-08-07 19:57:06 +0530

    copying the static navbar app in public

--- express_js/express_navbar.js
+++ express_js/express_navbar.js
@@ -2,11 +2,12 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+//setup static and middleware
 app.use(express.static('./public'))
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname, '../temp/navbar_app/index.html'))
-})
+// app.get('/',(req,res)=>{
+//     res.sendFile(path.resolve(__dirname, '../temp/navbar_app/index.html'))
+// })
 
 app.all('*',(req,res)=>{
     res.status(404).send('resource not found')
