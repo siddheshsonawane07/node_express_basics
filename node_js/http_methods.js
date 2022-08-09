171bd3c97b03eb72ab8829519f5e40d5a341c679
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-08-09 22:02:47 +0530

    http_methods example

---
+++ node_js/http_methods.js
@@ -0,0 +1,13 @@
+/**
+ * GET METHOD:  Read Data
+ * POST METHOD: Insert Data
+ * PUT METHOD: Update data
+ * DELETE METHOD: Delete data
+ * 
+ * GET     www.store.com/api/orders      get all orders
+ * POST    www.store.com/api/orders      place an order (send data)
+ * GET     www.store.com/api/orders/:id  get single order (path params)
+ * PUT     www.store.com/api/orders/:id  update specific order (params+send data)
+ * DELETE  www.store.com/api/orders/:id  delete specific order (path params)
+ * 
+ */
